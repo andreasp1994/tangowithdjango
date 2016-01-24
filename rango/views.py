5d26b343f721e7d5841bd8ff271cf4a87b8ae0ef
Andreas Pogiatzis <andreasp1994@gmail.com>
2016-01-24 20:51:12 +0200

    Completed 1-4 chapters code - first commit

---
+++ rango/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello world! <br/> <a href='/rango/about'>About</a>")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
