f4dfab6483057ec96bcef42260cd229761eedcad
Andreas Pogiatzis <andreasp1994@gmail.com>
2016-02-06 17:29:33 +0200

    Chapter 8 - Exercises completed - add_page added

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
 from rango.forms import CategoryForm
+from rango.forms import PageForm
 
 # Create your views here.
 def index(request):
@@ -31,6 +32,9 @@ def category(request, category_name_slug):
         # So the .get() method returns one model instance or raises an exception.
         category = Category.objects.get(slug=category_name_slug)
         context_dict['category_name'] = category.name
+
+        #Add category slug to context dict to pass it to add page
+        context_dict['category_slug'] = category_name_slug
 
         # Retrieve all of the associated pages.
         # Note that filter returns >= 1 model instance.
@@ -72,3 +76,29 @@ def add_category(request):
     # Bad form (or form details), no form supplied...
     # Render the form with error messages (if any).
     return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+                cat = None
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                # probably better to use a redirect here.
+                return category(request, category_name_slug)
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+
+    context_dict = {'form':form, 'category': cat, 'cat_slug':category_name_slug}
+
+    return render(request, 'rango/add_page.html', context_dict)
