215453e1dd334d330aa74c489570d481c1b95d83
Andreas Pogiatzis <andreasp1994@gmail.com>
2016-03-08 14:53:02 +0200

    Chapter 15 completed!

--- rango/views.py
+++ rango/views.py
@@ -9,7 +9,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from datetime import datetime
-
+from rango.bing_search import run_query
 def index(request):
 
     category_list = Category.objects.order_by('-likes')[:5]
@@ -140,3 +140,15 @@ def add_page(request, category_name_slug):
 def restricted(request):
     return render(request, 'rango/restricted.html', {})
 
+def search(request):
+
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            # Run our Bing function to get the results list!
+            result_list = run_query(query)
+
+    return render(request, 'rango/search.html', {'result_list': result_list})
